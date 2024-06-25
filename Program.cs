6e43fa505b2830d46aaef56d0bd6ea4334f02092
Thayssa <thayssavasconcelos01@gmail.com>
2024-06-24 23:08:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace aula_1
+{
+    public static class Program
+    {
+        public static void Main ()
+        {
+            Console.WriteLine("Olá, mundo");
+        }
+    }
+}
